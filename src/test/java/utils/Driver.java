1537dfcbb797540900921bb5e935cfef2e2869a0
Alec <ivnv.jr@gmail.com>
2020-08-27 21:39:35 -0400

    added sauce configrations

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -4,12 +4,24 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
 
 public class Driver {
     private static String propertyPath = "src/test/resources/conf/configuration.properties";
     private static WebDriver driver;
+
+    private static final String sauceUser = ConfigReader.readProperty("sauceUsername",propertyPath);
+    private static final String sauceKey = ConfigReader.readProperty("sauceKey", propertyPath);
+    private static final String URL = "https://" + sauceUser + ":" + sauceKey + "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
+
     public static WebDriver getDriver() {
         if (driver == null) {
+            if (ConfigReader.readProperty("executeTestLocally", propertyPath).equalsIgnoreCase("false"))
+                driver = getRemoteDriver();
+
             switch (ConfigReader.readProperty("browser", propertyPath)) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
@@ -29,4 +41,18 @@ public class Driver {
         getDriver().quit();
         driver = null;
     }
+    public static WebDriver getRemoteDriver(){
+        WebDriver driver = null;
+        try{
+            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+            capabilities.setCapability("browserName", "chrome");
+            capabilities.setCapability("version", ConfigReader.readProperty("browser_version", propertyPath));
+            capabilities.setCapability("platform", ConfigReader.readProperty("os", propertyPath));
+            driver = new RemoteWebDriver(new URL(URL),capabilities);
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        return driver;
+    }
+
 }
